7ab448709689f17e52adade957018e897820f5d5
Nicolas BONNIOT <nicolas@devgiants.fr>
2018-05-20 15:21:42 +0200

    Add SHutter named constructor for shutter without any button

--- src/Shutter.php
+++ src/Shutter.php
@@ -72,6 +72,46 @@ class Shutter {
 
 
 	/**
+	 * Creates one shutter with any physical button, just MQTT support
+	 *
+	 * @param GPO $outputOpen
+	 * @param GPO $outputClose
+	 * @param int $completeMovementDuration
+	 * @param LoopInterface $loop
+	 * @param MosquittoClientsReactWrapper $mqttClient
+	 * @param string $mqttTopic
+	 *
+	 * @return Shutter
+	 */
+	public static function createWithoutButtons(
+		GPO $outputOpen,
+		GPO $outputClose,
+		int $completeMovementDuration,
+		LoopInterface $loop,
+		MosquittoClientsReactWrapper $mqttClient,
+		string $mqttTopic
+	): Shutter {
+		// TODO checks
+
+		// Create shutter
+		$shutter = new static(
+			$outputOpen,
+			$outputClose,
+			$completeMovementDuration,
+			$loop,
+			$mqttClient,
+			$mqttTopic
+		);
+
+		// Register MQTT operations
+		$shutter->registerMqtt();
+
+		return $shutter;
+	}
+
+	/**
+	 * Creates one shutter with 2 buttons (one up, one down) and MQTT support
+	 *
 	 * @param GPI $inputOpen
 	 * @param GPI $inputClose
 	 * @param GPO $outputOpen
@@ -83,7 +123,7 @@ class Shutter {
 	 *
 	 * @return static
 	 */
-	public static function create(
+	public static function createWithTwoButtons(
 		GPI $inputOpen,
 		GPI $inputClose,
 		GPO $outputOpen,
@@ -97,14 +137,14 @@ class Shutter {
 
 		// Create shutter
 		$shutter = new static(
-			$inputOpen,
-			$inputClose,
 			$outputOpen,
 			$outputClose,
 			$completeMovementDuration,
 			$loop,
 			$mqttClient,
-			$mqttTopic
+			$mqttTopic,
+			$inputOpen,
+			$inputClose
 		);
 
 		$shutter->registerGpi(
@@ -126,24 +166,24 @@ class Shutter {
 	/**
 	 * Shutter constructor.
 	 *
-	 * @param GPI $inputOpen
-	 * @param GPI $inputClose
 	 * @param GPO $outputOpen
 	 * @param GPO $outputClose
 	 * @param int $completeMovementDuration
 	 * @param LoopInterface $loop
 	 * @param MosquittoClientsReactWrapper $mqttClient
 	 * @param string $mqttTopic
+	 * @param GPI $inputOpen
+	 * @param GPI $inputClose
 	 */
 	private function __construct(
-		GPI $inputOpen,
-		GPI $inputClose,
 		GPO $outputOpen,
 		GPO $outputClose,
 		int $completeMovementDuration,
 		LoopInterface $loop,
 		MosquittoClientsReactWrapper $mqttClient,
-		string $mqttTopic
+		string $mqttTopic,
+		GPI $inputOpen = NULL,
+		GPI $inputClose = NULL
 	) {
 		$this->inputOpen                = $inputOpen;
 		$this->inputClose               = $inputClose;
@@ -166,45 +206,49 @@ class Shutter {
 				$this->outputClose->reset();
 				$this->outputOpen->set();
 
-				$this->setTimerForEnding($this->outputOpen);
+				$this->setTimerForEnding( $this->outputOpen );
 
 			} else if ( static::CLOSE === $message ) {
 				$this->outputOpen->reset();
 				$this->outputClose->set();
 
-				$this->setTimerForEnding($this->outputClose);
+				$this->setTimerForEnding( $this->outputClose );
 			}
 		} );
 	}
 
 	/**
+	 * Listen to GPI change for shutter opening/closing
 	 * @param GPI $gpi
 	 * @param GPO $linkedGpo
 	 */
-	protected function registerGpi(GPI $gpi, GPO $linkedGpo): void {
-		$gpi->on(GPIO::AFTER_VALUE_CHANGE_EVENT, function() use($gpi, $linkedGpo) {
-			if($gpi->isSet()) {
-				$linkedGpo->set();
-				$this->setTimerForEnding($linkedGpo);
-			} else {
-				$linkedGpo->reset();
-				// Trigger timer cancellation to avoid unwanted effects if numerous input trigger
-				if(null !== $this->timer) {
-					$this->loop->cancelTimer($this->timer);
+	protected function registerGpi( GPI $gpi, GPO $linkedGpo ): void {
+		if ( NULL !== $gpi ) {
+			$gpi->on( GPIO::AFTER_VALUE_CHANGE_EVENT, function () use ( $gpi, $linkedGpo ) {
+				if ( $gpi->isSet() ) {
+					$linkedGpo->set();
+					$this->setTimerForEnding( $linkedGpo );
+				} else {
+					$linkedGpo->reset();
+					// Trigger timer cancellation to avoid unwanted effects if numerous input trigger
+					if ( NULL !== $this->timer ) {
+						$this->loop->cancelTimer( $this->timer );
+					}
 				}
-			}
-		});
+			} );
+		}
 	}
 
 
 	/**
+	 * Callback used for setting timer on operations
 	 * @param GPO $gpo
 	 */
-	protected function setTimerForEnding(GPO $gpo) : void {
-		if(null !== $this->timer) {
+	protected function setTimerForEnding( GPO $gpo ): void {
+		if ( NULL !== $this->timer ) {
 			$this->loop->cancelTimer( $this->timer );
 		}
-		$this->timer = $this->loop->addTimer( $this->completeMovementDuration, function () use ($gpo) {
+		$this->timer = $this->loop->addTimer( $this->completeMovementDuration, function () use ( $gpo ) {
 			$gpo->reset();
 		} );
 	}
